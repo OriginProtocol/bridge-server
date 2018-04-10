54a63f4886f074bd3f2062b916a3c7b523c2604f
Tyler Yasaka <hey@tyleryasaka.me>
2018-04-10 15:11:16 -0700

    Add email verification API endpoints

--- api/verification.py
+++ api/verification.py
@@ -20,8 +20,27 @@ class VerifyPhoneResponse(apilib.Response):
     claim_type = apilib.Field(apilib.String())
     data = apilib.Field(apilib.String())
 
+class GenerateEmailVerificationCodeRequest(apilib.Request):
+    eth_address = apilib.Field(apilib.String(), required=True)
+    email = apilib.Field(apilib.String(), required=True)
+
+class GenerateEmailVerificationCodeResponse(apilib.Response):
+    pass
+
+class VerifyEmailRequest(apilib.Request):
+    eth_address = apilib.Field(apilib.String(), required=True)
+    email = apilib.Field(apilib.String(), required=True)
+    code = apilib.Field(apilib.String(), required=True)
+
+class VerifyEmailResponse(apilib.Response):
+    signature = apilib.Field(apilib.String())
+    claim_type = apilib.Field(apilib.String())
+    data = apilib.Field(apilib.String())
+
 class VerificationService(apilib.Service):
     path = '/api/verification_service'
     methods = apilib.servicemethods(
-        apilib.Method('generate_phone_verification_code', GeneratePhoneVerificationCodeRequest, GeneratePhoneVerificationCodeResponse),
-        apilib.Method('verify_phone', VerifyPhoneRequest, VerifyPhoneResponse))
+    apilib.Method('generate_phone_verification_code', GeneratePhoneVerificationCodeRequest, GeneratePhoneVerificationCodeResponse),
+    apilib.Method('verify_phone', VerifyPhoneRequest, VerifyPhoneResponse),
+    apilib.Method('generate_email_verification_code', GenerateEmailVerificationCodeRequest, GenerateEmailVerificationCodeResponse),
+    apilib.Method('verify_email', VerifyEmailRequest, VerifyEmailResponse))
