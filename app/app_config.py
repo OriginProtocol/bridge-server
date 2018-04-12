b8f439a3bdf4144b0ee1648aac500ec7399ac1da
Tyler Yasaka <contact@tyleryasaka.me>
2018-04-12 15:35:52 -0700

    Set up session in app

--- app/app_config.py
+++ app/app_config.py
@@ -5,9 +5,11 @@ import flask_migrate
 
 from config import settings
 from database import db
+from flask.ext.session import Session
 
 class AppConfig(object):
     SECRET_KEY = settings.FLASK_SECRET_KEY
+    SESSION_TYPE = 'filesystem'
     CSRF_ENABLED = True
 
     SQLALCHEMY_DATABASE_URI = settings.SQLALCHEMY_DATABASE_URI
@@ -15,6 +17,8 @@ class AppConfig(object):
     SQLALCHEMY_ECHO = False
 
 def init_app(app):
+    sess = Session()
+    sess.init_app(app)
     db.init_app(app)
     flask_migrate.Migrate(app, db, directory='database/migrations')
 
