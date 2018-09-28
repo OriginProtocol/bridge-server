68a40f1b061524ab58a352d1eecef87ad817fe2c
tomlinton <tlinton@dockyard.nz>
2018-09-28 16:11:49 +1200

    Remove notifications

--- database/models.py
+++ database/models.py
@@ -18,19 +18,3 @@ class Attestation(db.Model):
     value = db.Column(db.String)
     signature = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-
-class EthNotificationTypes(Enum):
-    APN = 1  # Apple notification
-    FCM = 2  # Firebase cloud messaging
-
-
-class EthNotificationEndpoint(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    eth_address = db.Column(db.String(255), index=True)
-    device_token = db.Column(db.String(255), index=True)
-    type = db.Column(db.Enum(EthNotificationTypes))
-    active = db.Column(db.Boolean())
-    verified = db.Column(db.Boolean())
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    expires_at = db.Column(db.DateTime(timezone=True))
